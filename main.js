0f86a8b617e6984fe6834855f8810542e1fcf67f
Tiago Carioca <tiagoacarioca@gmail.com>
2019-11-16 00:51:04 -0400

    Aula Promises

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-let xhr = new XMLHttpRequest();
+let myPromise = function () {
+    return new Promise(function (resolve,reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET','https://api.github.com/users/tiagokriok');
+        xhr.send(null);
 
-xhr.open('GET','https://api.github.com/users/tiagokriok');
-xhr.send(null);
-
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4){
+                if (xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText));
+                }else{
+                    reject('Erro na requisição!!!');
+                }
+            }
+        }
+    });
 }
+
+myPromise()
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error);
+    });
