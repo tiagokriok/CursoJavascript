a3de9278334d90d2a3f198e7e430e65670ad2346
Tiago Carioca <tiagoacarioca@gmail.com>
2019-11-15 23:11:26 -0400

    Aula Iniciando aplicação

--- todos.js
+++ todos.js
@@ -1,1 +1,9 @@
-alert("TODO!");
+let listElement = document.querySelector('#app ul');
+let inputlement = document.querySelector('#app input');
+let buttonElement = document.querySelector('#app button');
+
+let todo = [
+    'Fazer café',
+    'Estudar Javascript',
+    'Acessar Comunidade da Rocketseat'
+];
